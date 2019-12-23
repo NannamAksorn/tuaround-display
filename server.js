02b97005e22e94c1fd8358c0a090a46b3338f2cb
mnphmnmn <nam_nam8@hotmail.com>
2019-12-23 22:00:57 +0700

    change dist

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 // server.js
-const expressStaticGzip =  require('express-static-gzip');
+const expressStaticGzip = require('express-static-gzip');
 const express = require('express');
-const favicon = require('express-favicon');
+// const favicon = require('express-favicon');
 const path = require('path');
+
+
 const port = process.env.PORT || 8080;
+
 const app = express();
+
 // app.use(favicon(__dirname + '/public/favicon.png'));
+
 // the __dirname is the current directory from where the script is running
+
 app.use(express.static(__dirname));// send the user to index html page inspite of the url
+
 app.use('/', expressStaticGzip(path.resolve(__dirname, 'dist'), {
   enableBrotli: true,
   orderPreference: ['br', 'gz'],
-  setHeaders: function (res, path) {
+  setHeaders(res) {
     res.setHeader('Cache-Control', 'public, max-age=31536000');
-  }
+  },
+
 }));
+
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
+
 app.listen(port);
